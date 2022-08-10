d4205f3972df04ef2d48819a2c4a5adc02be0215
GuentherJulian <julian.guenther@capgemini.com>
2022-08-10 18:48:37 +0200

    enriched parse tree matching

--- aim-pattern-detection/src/main/java/io/github/guentherjulian/masterthesis/patterndetection/engine/matching/ParseTreeMatcher.java
+++ aim-pattern-detection/src/main/java/io/github/guentherjulian/masterthesis/patterndetection/engine/matching/ParseTreeMatcher.java
@@ -1,6 +1,9 @@
 package io.github.guentherjulian.masterthesis.patterndetection.engine.matching;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -62,6 +65,7 @@ public class ParseTreeMatcher {
 
 		try {
 			matchOrderedPaths(orderedTemplatePaths, orderedCompilationUnitPaths);
+			matchUnorderedPaths(unorderedTemplatePaths, unorderedCompilationUnitPaths);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -91,11 +95,42 @@ public class ParseTreeMatcher {
 				}
 
 				parseTreeElementCompilationUnit = orderedCompilationUnitPaths.get(j);
-				matchPath(parseTreeElementTemplate, parseTreeElementCompilationUnit);
 
 				matches = true;
-				j++;
+				try {
+					matchPath(parseTreeElementTemplate, parseTreeElementCompilationUnit);
+				} catch (Exception e) {
+					matches = false;
+				}
+
+				if (!matches) {
+					j++;
+				} else {
+					j++;
+				}
 			} while (!matches);
+		}
+
+		return null;
+	}
+
+	private TreeMatch matchUnorderedPaths(List<ParseTreeElement> unorderedTemplatePaths,
+			List<ParseTreeElement> unorderedCompilationUnitPaths) {
+
+		Iterator<ParseTreeElement> iterator = unorderedTemplatePaths.iterator();
+		while (iterator.hasNext()) {
+			ParseTreeElement parseTreeElementTemplate = iterator.next();
+
+			for (ParseTreeElement parseTreeElementCompilationUnit : unorderedCompilationUnitPaths) {
+				TreeMatch match;
+				try {
+					match = matchPath(parseTreeElementTemplate, parseTreeElementCompilationUnit);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				// TODO evaluate match
+			}
 		}
 
 		return null;
@@ -108,8 +143,20 @@ public class ParseTreeMatcher {
 
 			if (!parseTreePathTemplate.isMetaLanguageElement()) {
 				if (parseTreePathTemplate.getText().equals(parseTreePathCompilationUnit.getText())) {
-					LOGGER.info("Match: {}, {}", parseTreePathTemplate.getText(),
+					LOGGER.info("New match 1: {}, {}", parseTreePathTemplate.getText(),
 							parseTreePathCompilationUnit.getText());
+					this.registerNewMatch(parseTreePathTemplate, parseTreePathCompilationUnit);
+				} else if (!parseTreePathTemplate.containsMetaLanguage()) {
+					throw new RuntimeException("Could not find " + parseTreePathTemplate.getPath() + " in "
+							+ parseTreePathCompilationUnit.getPath());
+				} else if (parseTreePathCompilationUnit.getPureObjLangPath()
+						.equals(parseTreePathTemplate.getPureObjLangPath())
+						&& parseTreePathCompilationUnit.getText().equals(parseTreePathTemplate.getText())) {
+					LOGGER.info("New match 2: {}, {}", parseTreePathTemplate.getText(),
+							parseTreePathCompilationUnit.getText());
+				} else {
+					throw new RuntimeException("Could not find " + parseTreePathTemplate.getPath() + " in "
+							+ parseTreePathCompilationUnit.getPath());
 				}
 			} else {
 				// Metalanguage code in template
@@ -117,7 +164,8 @@ public class ParseTreeMatcher {
 				ParseTreePath currentParseTreePathCompilationUnit = parseTreePathCompilationUnit;
 				do {
 					if (currentParseTreePathCompilationUnit == null) {
-						break;
+						throw new RuntimeException("Could not find " + parseTreePathTemplate.getPath() + " in "
+								+ parseTreePathCompilationUnit.getPath());
 					}
 
 					LOGGER.info("temp: {}, {}, {}, {}", currentParseTreePathTemplate.getText(),
@@ -128,8 +176,23 @@ public class ParseTreeMatcher {
 							currentParseTreePathCompilationUnit.getName(),
 							currentParseTreePathCompilationUnit.isMetaLanguageElement(),
 							currentParseTreePathCompilationUnit.containsMetaLanguage());
+
+					if (currentParseTreePathTemplate.getName().equals(currentParseTreePathCompilationUnit.getName())) {
+						currentParseTreePathCompilationUnit = currentParseTreePathCompilationUnit.getParent();
+					} else if (currentParseTreePathTemplate.isMetaLanguageElement()) {
+						if (currentParseTreePathTemplate.getParent().getName()
+								.matches("Fm_" + currentParseTreePathCompilationUnit.getName() + "Context")) {
+							LOGGER.info("New placeholder substitution {} {}", currentParseTreePathTemplate.getText(),
+									currentParseTreePathCompilationUnit.getText());
+							this.registerNewPlaceholderSubstitution(currentParseTreePathTemplate.getText(),
+									currentParseTreePathCompilationUnit.getText());
+							break;
+						}
+					} else {
+						currentParseTreePathCompilationUnit = currentParseTreePathCompilationUnit.getParent();
+					}
+
 					currentParseTreePathTemplate = currentParseTreePathTemplate.getParent();
-					currentParseTreePathCompilationUnit = currentParseTreePathCompilationUnit.getParent();
 				} while (currentParseTreePathTemplate != null);
 			}
 		} else if (templatePathElement instanceof ParseTreePathList
@@ -140,5 +203,24 @@ public class ParseTreeMatcher {
 			matchOrderedPaths(parseTreePathListTemplate, parseTreePathListCompilationUnit);
 		}
 		return null;
+	}
+
+	private void registerNewPlaceholderSubstitution(String key, String value) {
+		if (this.placeholderSubstitutions == null) {
+			this.placeholderSubstitutions = new HashMap<>();
+		}
+		if (!this.placeholderSubstitutions.containsKey(key)) {
+			Set<String> set = new HashSet<>();
+			set.add(value);
+			this.placeholderSubstitutions.put(key, set);
+		} else {
+			Set<String> set = placeholderSubstitutions.get(key);
+			set.add(value);
+			this.placeholderSubstitutions.put(key, set);
+		}
+	}
+
+	private void registerNewMatch(ParseTreePath parseTreePathTemplate, ParseTreePath parseTreePathCompilationUnit) {
+
 	}
 }
--- aim-pattern-detection/src/main/java/io/github/guentherjulian/masterthesis/patterndetection/parsing/ParseTreePath.java
+++ aim-pattern-detection/src/main/java/io/github/guentherjulian/masterthesis/patterndetection/parsing/ParseTreePath.java
@@ -1,5 +1,10 @@
 package io.github.guentherjulian.masterthesis.patterndetection.parsing;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class ParseTreePath implements ParseTreeElement {
 
 	private String text;
@@ -40,5 +45,44 @@ public class ParseTreePath implements ParseTreeElement {
 	@Override
 	public String toString() {
 		return getText();
+	}
+
+	public String getFullPathText() {
+		String fullText = "";
+		if (parent != null) {
+			fullText = parent.getFullPathText();
+		}
+		fullText += text;
+		return fullText;
+	}
+
+	public String getPath() {
+		return getPathSegments().stream().collect(Collectors.joining("/"));
+	}
+
+	public List<String> getPathSegments() {
+		List<String> pathSegments = new ArrayList<>();
+		if (parent != null) {
+			pathSegments.addAll(parent.getPathSegments());
+		}
+		pathSegments.add(name);
+		return pathSegments;
+	}
+
+	private List<ParseTreePath> getPathElements() {
+		List<ParseTreePath> pathElements = new ArrayList<>();
+		ParseTreePath currParent = parent;
+		while (currParent != null) {
+			pathElements.add(currParent);
+			currParent = currParent.parent;
+		}
+		// return in reverse order
+		return IntStream.rangeClosed(1, pathElements.size()).mapToObj(i -> pathElements.get(pathElements.size() - i))
+				.collect(Collectors.toList());
+	}
+
+	public Object getPureObjLangPath() {
+		return getPathElements().stream().filter(e -> !e.isMetaLanguageElement).map(ParseTreePath::getName)
+				.collect(Collectors.joining("/"));
 	}
 }
